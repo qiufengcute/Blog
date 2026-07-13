f2e31a08ca342b133db7fec0f4764b48b58f947f
Qiufeng <143275474+qiufengcute@users.noreply.github.com>
2026-07-13 17:00:47 +0800

    Create editingh.js

---
+++ source/js/editingh.js
@@ -0,0 +1,8 @@
+(function() {
+  const path = window.location.pathname;
+  const editingh_ele = document.getElementById("editingh");
+  if (path.startsWith("/Blog/posts/") && path != "/Blog/posts/") {
+    editingh_ele.hidden = false;
+    editingh_ele.href = `https://github.com/qiufengcute/Blog/source/_posts/${path.split("/Blog/posts/")[1]}`;
+  }
+})();
