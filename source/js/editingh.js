943d3d588afce6050c08aa2199f41e192e133e35
Qiufeng <143275474+qiufengcute@users.noreply.github.com>
2026-07-13 17:21:14 +0800

    Update editingh.js

--- source/js/editingh.js
+++ source/js/editingh.js
@@ -3,6 +3,10 @@
   const editingh_ele = document.getElementById("editingh");
   if (path.startsWith("/Blog/posts/") && path != "/Blog/posts/") {
     editingh_ele.hidden = false;
-    editingh_ele.href = `https://github.com/qiufengcute/Blog/source/_posts/${path.split("/Blog/posts/")[1].slice(0, -1)}.md/`;
+    editingh_ele.href = `https://github.com/qiufengcute/Blog/blob/master/source/_posts/${path.split("/Blog/posts/")[1].slice(0, -1)}.md/`;
+  }
+  if (path == "/Blog/about/") {
+    editingh_ele.hidden = false;
+    editingh_ele.href = `https://github.com/qiufengcute/Blog/blob/master/source/about/index.md/`;
   }
 })();
